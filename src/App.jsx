7ca74fc7c3ec0dd25a744c99b6a4450b5e3345de
lord-amresh <amresh2006@gmail.com>
2026-03-18 09:12:42 +0545

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+    <h1 className="text-blue-600 dark:text-sky-400 border-2 p-4 rounded-xl">Learn to integrate tailwind</h1>
+    </>
+  )
+}
+
+export default App
