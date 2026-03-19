fc2a13b8552b1553dca7f73e3a1b36a8a1a58435
lord-amresh <amresh2006@gmail.com>
2026-03-19 20:49:23 +0545

    Finished Cards components with custom props

--- src/App.jsx
+++ src/App.jsx
@@ -7,9 +7,15 @@ function App() {
     <h1 className="text-blue-600 dark:text-sky-400 border-2 p-4 rounded-xl">Learn to integrate tailwind</h1>
     
     <div className='flex gap-4' >
-      <Card title= "Buy Python Course"/>
-      <Card title= "Buy Node Js Course"/>
-      <Card/>
+      <Card title= "Buy Python Course" 
+      buttonText="Join Now"
+      imageUrl="https://images.unsplash.com/photo-1509042239860-f550ce710b93"
+      />
+      <Card title= "Buy Node Js Course"
+      imageUrl= "https://images.pexels.com/photos/7038220/pexels-photo-7038220.jpeg" 
+      />
+      <Card imageUrl = "https://images.pexels.com/photos/7148531/pexels-photo-7148531.jpeg" 
+      />
 
     </div>
     
